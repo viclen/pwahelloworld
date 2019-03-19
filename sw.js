d74204d2c123ef38e25d7de416435477360eda8a
viclen <33470132+viclen@users.noreply.github.com>
2019-03-18 21:14:10 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -14,14 +14,15 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v1';
+const PRECACHE = 'testezinho';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   'index.html',
   './', // Alias for index.html
-  'demo.js'
+  'demo.js',
+  'app.js'
 ];
 
 // The install handler takes care of precaching the resources we always need.
